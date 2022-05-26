baea29964e7966eb00d7145f9ca27c5110936409
Jose Duarte <2211853@my.ipleiria.pt>
2022-05-26 12:07:58 +0100

    Base MVC

---
+++ weblogicmvc/controllers/BaseAuthController.php
@@ -0,0 +1,30 @@
+<?php
+require_once './controllers/BaseController.php';
+require_once './models/Auth.php';
+
+class BaseAuthController extends BaseController
+{
+    protected function loginFilter()
+    {
+        $auth = new Auth();
+
+        if(!$auth->isLoggedIn())
+        {
+            header('Location: ./router.php?' . INVALID_ACCESS_ROUTE);
+        }
+    }
+
+    protected function loginFilterByRole($roles=[])
+    {
+        $auth = new Auth();
+
+        $role = $auth->getRole();
+        $validRole = in_array($role,$roles);
+
+        if(!$validRole)
+        {
+            header('Location: ./router.php?' . INVALID_ACCESS_ROUTE);
+        }
+    }
+
+}
